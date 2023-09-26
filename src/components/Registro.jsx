336e42f86f1c5546cbf0909717ce0834338adc78
francisco cifuentes <francisco.cif85@gmail.com>
2023-09-25 22:05:30 -0300

    registro

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -3,21 +3,27 @@ import Alerta from "./Alert";
 import Formulario from "./Formulario";
 import SocialButton from "./SocialButton";
 
-
 export const Registro = () => {
-    const [alert, setAlert] = useState({msg: '', color: ''})
+  const [alert, setAlert] = useState({ msg: '', color: '' });
 
-    return(
-       <>
-            <div className="registro">
-                <SocialButton
-                    iconoFace = {<a href="https://www.facebook.com/home.php"><i className="fa-brands fa-facebook fa-lg"></i></a>}
-                    iconoGit = {<a href="https://github.com/Nuncia"><i className="fa-brands fa-github fa-lg"></i></a>}
-                    iconoLink = {<a href="https://www.linkedin.com/feed/"><i className="fa-brands fa-linkedin-in fa-lg"></i></a>}
-                />
-                <Formulario setAlert={setAlert}/>
-                    {alert.msg && <Alerta colorAlert={alert.color} textoAlert={alert.msg}/>}
-            </div>
-       </> 
-    );
+  const handleSubmit = (formData) => {
+   
+    console.log('Form Data:', formData);
+
+ 
+  };
+
+  return (
+    <>
+      <div className="registro">
+        <SocialButton
+          iconoFace={<a href="https://www.facebook.com/home.php"><i className="fa-brands fa-facebook fa-lg"></i></a>}
+          iconoGit={<a href="https://github.com/Nuncia"><i className="fa-brands fa-github fa-lg"></i></a>}
+          iconoLink={<a href="https://www.linkedin.com/feed/"><i className="fa-brands fa-linkedin-in fa-lg"></i></a>}
+        />
+        <Formulario onSubmit={handleSubmit} setAlert={setAlert} />
+        {alert.msg && <Alerta colorAlert={alert.color} textoAlert={alert.msg} />}
+      </div>
+    </>
+  );
 };
